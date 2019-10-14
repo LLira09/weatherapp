d2c69d43b4cb515a904d2b6fb27dfd5f26114641
Larry Lira <larrylira@larrys-mbp.attlocal.net>
2019-10-14 18:25:16 -0500

    added styling

--- weather/lookup/urls.py
+++ weather/lookup/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('lookup.urls')),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    
 ]
